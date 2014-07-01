43ebb8e689a4e86b9472acb6698abdf9b9a35a3a
Kieldro <kieldro@gmail.com>
2014-06-30 19:58:42 -0500

    entropy computed

--- Encoder.java
+++ Encoder.java
@@ -1,34 +1,60 @@
 import java.io.*;
 import java.util.*;
+// import static java.lang.Math.*;
 
 class Encoder{
     static boolean DEBUG = true;
-    HashMap<Character, Double> probabilities = new HashMap();
+    static HashMap<Character, Double> probabilities = new HashMap();
+    static LinkedList<Double> list = new LinkedList();
     
-    public static void main (String[] args){
+    public static void main (String[] args) throws Exception{
         Random gen = new Random();
         int k = Integer.parseInt(args[1]);
-
+        Scanner sc = new Scanner(new File(args[0]));
+        
         if (DEBUG) System.out.println("k: " + k);
+        
+        int sum = 0;
+        while( sc.hasNext() ){
+            double x = sc.nextInt();
+            list.push(x);
+            sum += x;
+            if (DEBUG) System.out.println("x: " + x);
+        }
+        if (DEBUG) System.out.println("sum: " + sum);
+        
+        for(ListIterator<Double> it = list.listIterator(); it.hasNext(); ){
+            double x = it.next();
+            it.set(x / (double)sum);
+        }
+        if (DEBUG) System.out.println("list: " + list);
+        
         
         for(int i = 0; i < 10; ++i){
         	System.out.println("random: " + gen.nextInt());
         }
+        
         Encoder e = new Encoder();
-        e.computeEntropy();
+        double h = e.computeEntropy();
+        if (DEBUG) System.out.println("h: " + h + " bits");
+        
+        
     }
     
-    // h = Sigma(P * log P)
+    // h = - Sigma(P * log P)
     double computeEntropy(){
-    	double h = 3;
-    	probabilities.put('a', 4.0);
-    	probabilities.put('b', 3.0);
-    	probabilities.put('c', 2.0);
-    	probabilities.put('d', 1.0);
+    	double h = 0;
+    	probabilities.put('a', .40);
+    	probabilities.put('b', .30);
+    	probabilities.put('c', .20);
+    	probabilities.put('d', .10);
     	
-    	h = 0;
+        for(Double prob : list){
+    	   h -= prob * Math.log(prob) / Math.log(2);
+        }
     	
-    	
+    	// h *= -1;
+        
     	return h;
     }
     
